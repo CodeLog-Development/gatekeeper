11a3dfcd53041d0b13e3fc9135f075fb002052a0
Jaco Malan <jacomalan.12@gmail.com>
2023-10-01 01:19:28 +0200

    Added user creation endpoint

--- api/src/lib/api.module.ts
+++ api/src/lib/api.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ServerModule } from './server/server.module';
+import { FirebaseModule } from './firebase/firebase.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  providers: [],
-  imports: [ServerModule],
-  exports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    ServerModule,
+    FirebaseModule,
+    UserModule,
+  ],
 })
 export class ApiModule { }
