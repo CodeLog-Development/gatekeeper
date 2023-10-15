5799295a940f48f18d75a2004267868c0b522825
Jaco Malan <jacomalan.12@gmail.com>
2023-10-15 13:31:46 +0200

    Re-enable shop page

--- app/src/app/tabs/tabs-routing.module.ts
+++ app/src/app/tabs/tabs-routing.module.ts
@@ -17,6 +17,11 @@ const routes: Routes = [
         loadChildren: () =>
           import('../profile/profile.module').then((m) => m.ProfilePageModule),
       },
+      {
+        path: 'shop',
+        loadChildren: () =>
+          import('../shop/shop.module').then((m) => m.ShopPageModule),
+      },
     ],
   },
 ];
